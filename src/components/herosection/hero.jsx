1507a7a9b623e7be8393827ccd7c7a309b800b97
Khizar Wakeel <khizarwakeel@gmail.com>
2023-10-21 20:23:53 +0500

    added changes and 3d Model

--- src/components/herosection/hero.jsx
+++ src/components/herosection/hero.jsx
@@ -1,12 +1,13 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import Boxdemo from "../boxdemo";
 import { OrbitControls } from "@react-three/drei";
+import { Model } from "../3D-Model/Scene";
+
 const Hero = () => {
   return (
     <div className="pt-14 w-[90%] mx-auto">
-      <div className="p-10 lg:h-[84vh] lg:bg-gradient-to-r from-[#4e1daa] via-[#8b11d6] to-[#9a10e2] lg:shadow-lg lg:rounded-3xl">
-        <div className="grid lg:grid-cols-2 gap-10">
+      <div className="p-10 lg:bg-gradient-to-r from-[#4e1daa] via-[#8b11d6] to-[#9a10e2] lg:shadow-lg lg:rounded-3xl">
+        <div className="grid py-10 lg:grid-cols-2 gap-10">
           <div className=" text-white">
             <h1 className="text-xl md:text-5xl uppercase">Stixor</h1>
             <h1 className="text-xl md:text-5xl uppercase">Technologies</h1>
@@ -14,14 +15,14 @@ const Hero = () => {
               We come up with creative solutions using the latest technologies
             </p>
           </div>
-          {/* <div className="h-96">
-            <Canvas className="canvas">
+          <div className="h-96">
+            <Canvas>
               <OrbitControls enableZoom={true} />
               <ambientLight intensity={0.5} />
-              <directionalLight position={[2, 2, 2]} intensity={2} />
-              <Boxdemo />
+              <directionalLight position={[0, 0, 0]} intensity={1} />
+              <Model />
             </Canvas>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>

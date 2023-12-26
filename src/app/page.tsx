bf1d997232ffe8d3cc7c31efcff6a51ea30f651b
Khizar Wakeel <khizarwakeel@gmail.com>
2023-12-26 19:54:42 +0500

    Initialized New Project and added TypeScript and Next 14

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Index from "@/components"
+
+const Page = () => {
+  return (
+    <div>
+      <Index />
+    </div>
+  )
+}
+
+export default Page
---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+
+const Index = () => {
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center h-screen text-4xl">
+      Comming Soon!
+    <h2 className="text-xl">Made by Khizar Wakeel</h2>
+    </div>
+  )
+}
+
+export default Index

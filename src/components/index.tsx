6c3ac2c307154cccb0904aebe3aead44168e594f
Khizar Wakeel <khizarwakeel@gmail.com>
2024-01-05 21:22:35 +0500

    added new line demo

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,6 +3,7 @@ const Index = () => {
     <section className="flex flex-col gap-5 justify-center items-center h-screen text-4xl">
       <h1>Comming Soon!</h1>
       <h3 className="text-xl">Made by Khizar Wakeel</h3>
+      <h4>Software Developer from Pakistan</h4>
     </section>
   );
 };
